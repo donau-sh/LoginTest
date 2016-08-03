190472c1462224baebccee77d4c581694c82d98d
Beiqi Lue <blue@pc22.local>
2016-08-03 13:59:06 +0200

    change .idea to .gitignore; refactory MainActivity to LoginActivity

---
+++ app/src/main/java/de/gm_w/android/logintest/LoginActivity.java
@@ -0,0 +1,13 @@
+package de.gm_w.android.logintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
